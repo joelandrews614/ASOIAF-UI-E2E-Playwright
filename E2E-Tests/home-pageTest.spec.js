ab85de762ad5538397b1b56ab1fcffc62dfb86ed
lultr <joelandrews614@gmail.com>
2024-06-16 19:29:20 +0530

    Added the #2 Test in Home Page Scenarios

--- E2E-Tests/home-pageTest.spec.js
+++ E2E-Tests/home-pageTest.spec.js
@@ -1,9 +1,20 @@
 const {test,expect} = require('@playwright/test');
 
-test('Home Page Validation', async ({page}) => {
+test("Home Page Validation", async ({page}) => {
 
     await page.goto("https://anapioficeandfire.com/");
 
     await expect(page).toHaveTitle("An API of Ice And Fire");
+
+    await expect(page).toHaveURL("https://anapioficeandfire.com/");
+})
+
+test("Validate Author's Website Link", async ({page}) => {
+
+    await page.goto("https://anapioficeandfire.com/");
+    
+    await page.locator("//section/p/a[@href='https://joakimskoog.com/']").click();
+
+    await page.locator("//a[@href='/']").click();
 
 })
